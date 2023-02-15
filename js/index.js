d59bf82cf4056be32932ce5846fbe696f5c21059
Lasten-Ernest <lastenernest@gmail.com>
2023-02-15 13:52:11 +0200

    update js and html files

--- js/index.js
+++ js/index.js
@@ -102,15 +102,12 @@ function handlePageChange(e) {
   if (e.target.classList.contains('list')) {
     document.querySelector('#book-display').style.display = 'block';
     document.querySelector('#add-book').style.display = 'none';
-    document.querySelector('#contact').style.display = 'none';
   } else if (e.target.classList.contains('addBook')) {
     document.querySelector('#book-display').style.display = 'none';
     document.querySelector('#add-book').style.display = 'block';
-    document.querySelector('#contact').style.display = 'none';
   } else if (e.target.classList.contains('contactInfo')) {
     document.querySelector('#book-display').style.display = 'none';
     document.querySelector('#add-book').style.display = 'none';
-    document.querySelector('#contact').style.display = 'block';
   }
 }
 
